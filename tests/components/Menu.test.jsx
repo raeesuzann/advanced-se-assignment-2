f04855debc26710ceaa8f999f58ff9572f99fca6
shyam kumar <shyamkumar@shyams-MacBook-Pro.local>
2026-04-25 19:04:02 +0100

    congigure unit test

---
+++ tests/components/Menu.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Menu from "../../src/components/Menu";
+
+describe("Menu", () => {
+    it("Should render the main menu with title", () => {
+        render(<Menu />);
+        expect(screen.getByText('Tic Tac Toe')).toBeInTheDocument();
+    })
+
+    it("Should renders both game mode button", () => {
+        render(<Menu />);
+        screen.debug()
+        expect(screen.getByRole("button", {name: /local multiplayer/i })).toBeInTheDocument();
+        expect(screen.getByRole("button", {name: /play vs computer/i })).toBeInTheDocument();
+    })
+})
