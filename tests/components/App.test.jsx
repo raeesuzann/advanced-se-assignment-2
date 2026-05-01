57228d74f9282c5d31f83c382c99526152bec2a9
shyam kumar <shyamkumar@shyams-MacBook-Pro.local>
2026-05-02 00:42:27 +0100

    tdd:player turn

--- tests/components/App.test.jsx
+++ tests/components/App.test.jsx
@@ -1,7 +1,6 @@
 import { describe, expect, it } from "vitest";
 import App from "../../src/App";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 describe("App", () => {
     it("Should renders heading", () => {
@@ -11,14 +10,4 @@ describe("App", () => {
 
         expect(heading).toBeInTheDocument();
     });
-
-    it("Should starts local game when clicking start local", async () => {
-        const user = userEvent.setup();
-        render(<App />);
-
-        await user.click(screen.getByRole("button", { name: /local/i }));
-
-        expect(screen.getByText(/local match/i)).toBeInTheDocument();
-    });
-    
 });
